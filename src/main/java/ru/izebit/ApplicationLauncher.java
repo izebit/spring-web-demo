c3ba74a9f3cf83780e5f468cce300bb986add1d8
Artem Konovalov <izebit@gmail.com>
2017-06-25 11:24:48 +0300

    шаг 2 - создание приложения spring boot

---
+++ src/main/java/ru/izebit/ApplicationLauncher.java
@@ -0,0 +1,16 @@
+package ru.izebit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:izebit@gmail.com">Artem Konovalov</a> <br/>
+ *         Creation date: 6/25/17.
+ * @since 1.0
+ */
+@SpringBootApplication
+public class ApplicationLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationLauncher.class, args);
+    }
+}
